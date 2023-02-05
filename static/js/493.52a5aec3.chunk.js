3fcc16c001984317104df394d02cb8a2aa85fb46
DimaKrupenko <105938881+DimaKrupenko@users.noreply.github.com>
2023-02-05 13:24:25 +0000

    Deploying to gh-pages from @ DimaKrupenko/goit-react-hw-08-phonebook@2f305ec78215d06414583eef64cd370cc8685fd2 🚀

--- static/js/493.52a5aec3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[493],{5493:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(184),l={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:l.container,children:(0,i.jsx)("h1",{style:l.title,children:"\u041dello, welcome to the phone book"})})}}}]);
-//# sourceMappingURL=493.52a5aec3.chunk.js.map
